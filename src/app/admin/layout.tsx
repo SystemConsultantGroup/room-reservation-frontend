630bd7cefa1a5187aa82ad06e9ec9cfa39a55021
snghyn28 <qazxsw100415@gmail.com>
2026-04-08 20:36:33 +0900

    feat: 어드민 페이지에 metatdata 제거

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,9 +1,4 @@
-import type { Metadata } from 'next';
 import { AdminLayoutWrapper } from '@/components/layout/admin/AdminLayoutWrapper';
-
-export const metadata: Metadata = {
-  title: 'ACADEMIC ATELIER - 관리자 패널',
-};
 
 export default function AdminLayout({
   children,
